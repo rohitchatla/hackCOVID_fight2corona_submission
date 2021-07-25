22cdb6362b74d597350d49cec51922fd4d34c706
rohit <rohitchatla@gmail.com>
2021-07-25 23:06:54 +0530

    init

---
+++ handdetect/app.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+const modelParams = {
+  flipHorizontal: true,   // flip e.g for video 
+  imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+  maxNumBoxes: 20,        // maximum number of boxes to detect
+  iouThreshold: 0.5,      // ioU threshold for non-max suppression
+  scoreThreshold: 0.79,    // confidence threshold for predictions.
+}
+
+
+
+navigator.getUserMedia=navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia||navigator.msGetUserMedia;
+
+//Select everthing in html
+
+const video=document.querySelector('#video');
+const audio=document.querySelector('#audio');
+const canvas=document.querySelector('#canvas');
+const context=canvas.getContext('2d');
+let model;
+
+
+handTrack.startVideo(video).then(status =>{
+
+	if(status){
+		navigator.getUserMedia(
+			{video:{} },
+			stream =>{
+			video.srcObject =stream;//observer is webcam which observes the surroundings
+			setInterval(runDetection,1000);
+			runDetection();//for multiple times running
+
+		},
+		err =>console.log(err)
+		);
+
+	}
+});
+
+	
+
+function runDetection(){
+
+	model.detect(video)
+	.then(predictions =>{
+		console.log(predictions);
+		model.renderPredictions(predictions,canvas,context,video);
+		if(predictions.length>0){
+			//quickstart();
+			audio.play();
+
+
+		}else{
+
+			audio.pause();
+		}
+		requestAnimationFrame(runDetection);//for multiple times running
+	});
+}
+
+handTrack.load(modelParams).then(lmodel =>{
+model=lmodel;
+});
